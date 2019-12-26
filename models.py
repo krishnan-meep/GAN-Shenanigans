57f9c1e4bc0a5d1dae599e76af630d8e4b8d8681
krishnanmeep <96meep96@gmail.com>
2019-12-26 22:42:52 +0530

    Minor corrections in CIFAR SNGAN model

--- models.py
+++ models.py
@@ -9,28 +9,28 @@ from building_blocks import ResBlock, UpBlock, DownBlock, ModulatingNet, Minibat
 ###########################################
 #Generator Network#########################
 class Generator(nn.Module):
-	#Image size should be a tuple, preferably a multiple of 16 
+	#Image size should be a tuple, preferably a multiple of 8
 	def __init__(self, image_size, noise_dim = 128, specnorm = False):
 		super(Generator, self).__init__()
-		self.h, self.w = image_size[0]//16, image_size[1]//16
+		self.h, self.w = image_size[0]//8, image_size[1]//8
 
-		self.Proj = nn.Linear(noise_dim, self.h*self.w*1024)
-		self.B0 = nn.BatchNorm2d(1024)
-		self.MN0 = ModulatingNet(1024, specnorm = specnorm)
+		self.Proj = nn.Linear(noise_dim, self.h*self.w*512)
+		self.B0 = nn.BatchNorm2d(512)
+		self.MN0 = ModulatingNet(512, specnorm = specnorm)
 
-		self.TC1 = nn.ConvTranspose2d(1024, 512, kernel_size = 4, stride = 2, padding = 1)
-		self.B1 = nn.BatchNorm2d(512)
-		self.MN1 = ModulatingNet(512, specnorm = specnorm)
+		self.TC1 = nn.ConvTranspose2d(512, 256, kernel_size = 4, stride = 2, padding = 1)
+		self.B1 = nn.BatchNorm2d(256)
+		self.MN1 = ModulatingNet(256, specnorm = specnorm)
 
-		self.TC2 = nn.ConvTranspose2d(512, 256, kernel_size = 4, stride = 2, padding = 1)
-		self.B2 = nn.BatchNorm2d(256)
-		self.MN2 = ModulatingNet(256, specnorm = specnorm)
+		self.TC2 = nn.ConvTranspose2d(256, 128, kernel_size = 4, stride = 2, padding = 1)
+		self.B2 = nn.BatchNorm2d(128)
+		self.MN2 = ModulatingNet(128, specnorm = specnorm)
 
-		self.TC3 = nn.ConvTranspose2d(256, 128, kernel_size = 4, stride = 2, padding = 1)
-		self.B3 = nn.BatchNorm2d(128)
-		self.MN3 = ModulatingNet(128, specnorm = specnorm)
+		self.TC3 = nn.ConvTranspose2d(128, 64, kernel_size = 4, stride = 2, padding = 1)
+		self.B3 = nn.BatchNorm2d(64)
+		self.MN3 = ModulatingNet(64, specnorm = specnorm)
 
-		self.TC4 = nn.ConvTranspose2d(128, 3, kernel_size = 4, stride = 2, padding = 1)
+		self.TC4 = nn.ConvTranspose2d(64, 3, kernel_size = 3, stride = 1, padding = 1)
 
 		if specnorm:
 			self.Proj = SpectralNorm(self.Proj)
@@ -41,7 +41,7 @@ class Generator(nn.Module):
 	 
 	def forward(self, z):
 		x = self.Proj(z)
-		x = x.view(-1, 1024, self.h, self.w)
+		x = x.view(-1, 512, self.h, self.w)
 		x = self.B0(x)
 		g, b = self.MN0(z)
 		x = g*x + b
@@ -74,15 +74,14 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
 	def __init__(self, image_size):
 		super(Discriminator, self).__init__()
-		self.h, self.w = image_size[0]//32, image_size[1]//32
+		self.h, self.w = image_size[0]//16, image_size[1]//16
 
 		self.DB1 = DownBlock(3, 64, specnorm = True, batchnorm = False)
 		self.DB2 = DownBlock(64, 128, specnorm = True, batchnorm = False)
 		self.DB3 = DownBlock(128, 256, specnorm = True, batchnorm = False)
 		self.DB4 = DownBlock(256, 512, specnorm = True, batchnorm = False)
-		self.DB5 = DownBlock(512, 512, specnorm = True, batchnorm = False)
-        
-		self.D1 = SpectralNorm(nn.Linear(512*self.h*self.w + 512, 1))
+
+		self.D1 = SpectralNorm(nn.Linear(512*self.h*self.w, 1))
 		self.MBD = MinibatchDiscrimination1d(512*self.h*self.w, 512)
 
 	def forward(self, x):
@@ -94,9 +93,8 @@ class Discriminator(nn.Module):
 		x = self.DB2(x)
 		x = self.DB3(x)
 		x = self.DB4(x)
-		x = self.DB5(x)
 		x = x.view(-1, 512*self.h*self.w)
-		x = self.MBD(x)
+		#x = self.MBD(x)
 		x = self.D1(x)
 		return x
 
